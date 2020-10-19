66f47341ed0cbad68f647251cca81da4c25ec607
Kayla <kaylacharky@hotmail.com>
2020-10-19 00:15:46 -0400

    #25 Delivery 1: Add Temperature to Model

--- smart-home-simulator/src/main/java/smarthomesimulator/model/Simulator.java
+++ smart-home-simulator/src/main/java/smarthomesimulator/model/Simulator.java
@@ -2,10 +2,6 @@ package smarthomesimulator.model;
 
 import java.util.ArrayList;
 
-/**
- *
- */
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
@@ -15,14 +11,17 @@ public class Simulator {
 
     private String date;
     private String time;
+    private int tempOut;
+    private int tempIn;
 
     public static ArrayList<Room> roomsOfHouse = new ArrayList<Room>();
-
 
     public Simulator() {
         super();
         this.date = this.getCurrentDate();
         this.time = this.getCurrentTime();
+        this.tempIn = this.getTempIn();
+        this.tempOut = this.getTempOut();
     }
 
     public void setDate(String date) {
@@ -40,6 +39,14 @@ public class Simulator {
     public String getTime() {
         return time;
     }
+
+    public int getTempOut() { return tempOut; }
+
+    public void setTempOut(int tempOut) { this.tempOut = tempOut; }
+
+    public int getTempIn() { return tempIn; }
+
+    public void setTempIn(int tempIn) { this.tempIn = tempIn; }
 
     public String getCurrentDate(){
 
