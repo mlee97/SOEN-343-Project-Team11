1819010b8c08e9a54807f73923e73188512d6481
Niels <neils_justin@hotmail.com>
2020-11-04 19:37:45 -0500

    D2 - #38 Added model icons to House Layout

--- smart-home-simulator/src/main/java/smarthomesimulator/model/Room.java
+++ smart-home-simulator/src/main/java/smarthomesimulator/model/Room.java
@@ -30,12 +30,85 @@ public class Room {
 				windows.add(i, new Window());
 			}
 		}
+		
+		public void initLights(int numOfLights) {
+			for(int i = 0; i < numOfLights ; i++){
+				lights.add(i, new Light());
+			}
+		}
 
 	    public void setRoomName(String roomName) { this.roomName=roomName; }
-	    public void setLights(int lights) { this.numOfLights=lights; }
 	    
 	    public String getRoomName() { return this.roomName; }
-	    public int getLights() { return this.numOfLights; }
+	     
+	    public boolean canEnter() {
+	    	
+	    	boolean canEnter = false;
+	    	
+	    	for(int i=0; i>=doors.size(); i++) {
+	    		if(canEnter == true) {
+	    			break;
+	    		}
+	    		if(doors.get(i).isOpen()) {
+	    			canEnter = true;
+	    		}
+	    	}
+	    	
+	    	return canEnter;
+			
+	    }
+	    
+	    public boolean isWindy() {
+	    	boolean windy = false;
+	    	
+	    	for(int i=0; i>=windows.size(); i++) {
+	    		if(windy == true) {
+	    			break;
+	    		}
+	    		if(windows.get(i).isOpen()) {
+	    			windy = true;
+	    		}
+	    	}
+	    	return windy;
+	    }
+	    
+	    public boolean isBright() {
+	    	
+	    	boolean bright = false;
+	    	
+	    	for(int i=0; i>=lights.size(); i++) {
+	    		if(bright == true) {
+	    			break;
+	    		}
+	    		if(lights.get(i).OnOff()) {
+	    			bright = true;
+	    		}
+	    	}
+	    	
+	    	return bright; 
+	    }
+	    
+	    public boolean findDoors() {
+	    	if (doors.size() == 0) {
+	    		return false;
+	    	}else
+	    		return true;
+	    }
+	    
+	    public boolean findLights() {
+	    	if (lights.size() == 0) {
+	    		return false;
+	    	}else
+	    		return true;
+	    }
+	    
+	    public boolean findWindows() {
+	    	if (windows.size() == 0) {
+	    		return false;
+	    	}else
+	    		return true;
+	    }
+	    
 	    
 	}
 
