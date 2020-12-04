d0570b2f61f149113c644d685f1eac24a2e76533
Niels <neils_justin@hotmail.com>
2020-12-03 22:21:14 -0500

    Added garage, backyard and entrance

--- smart-home-simulator/src/main/java/smarthomesimulator/RoomController.java
+++ smart-home-simulator/src/main/java/smarthomesimulator/RoomController.java
@@ -1,25 +1,14 @@
 package smarthomesimulator;
 
-
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import smarthomesimulator.model.Door;
-import smarthomesimulator.model.Profile;
-import smarthomesimulator.model.Room;
 import smarthomesimulator.model.Simulator;
 
 @Controller
